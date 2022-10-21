cea00cb03cfd6cca2805535501ce56338f6b1a1e
NicolaeArhire <nicolae.arhire10@gmail.com>
2022-10-21 16:50:18 +0300

    Game checkboxes

--- game.js
+++ game.js
@@ -20,3 +20,28 @@ if (currentday.getMonth()==2 && currentday.getDate()>14)
 
 const day = 1000*60*60*24;
 math.textContent = Math.abs(Math.ceil((math2.getTime()-currentday.getTime())/(day))+366) + ' days left until Maths Day!';
+
+
+let magic = document.getElementById('secret');
+let done = document.getElementById('done');
+let card1 = document.getElementById('c1').value = 1;
+let card2 = document.getElementById('c2').value = 2;
+let card3 = document.getElementById('c3').value = 4;
+let card4 = document.getElementById('c4').value = 8;
+let card5 = document.getElementById('c5').value = 16;
+let card6 = document.getElementById('c6').value = 32;
+
+if (done.checked) {
+    magic.textContent = '?????' + '. Awesome, right?';
+} else {
+    magic.textContent = '';
+}
+
+console.log(card1.checked);
+
+function uncheckAll (chk) {
+    for (i=0; i<chk.length; i++) {
+        chk[i].checked = false;
+    }
+}
+
