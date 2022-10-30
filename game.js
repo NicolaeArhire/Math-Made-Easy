9d3430f317b93b8693cbf0e6248bbe162c1984ac
NicolaeArhire <nicolae.arhire10@gmail.com>
2022-10-30 17:38:24 +0200

    Files Validated by W3

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ let time = document.getElementById('current-time');
 setInterval(() =>{
     let d = new Date()
     time.innerHTML = d.toLocaleTimeString();
-},1000)
+},1000);
 
 let date = document.getElementById('current-date');
 let dd = new Date()
@@ -15,8 +15,8 @@ let math2 = new Date(currentday.getFullYear(), 2, 14);
 
 if (currentday.getMonth()==2 && currentday.getDate()>14)
 {
-    math2.setFullYear(math2.getFullYear()+1)
-}
+    math2.setFullYear(math2.getFullYear()+1);
+};
 
 const day = 1000*60*60*24;
 math.textContent = Math.abs(Math.ceil((math2.getTime()-currentday.getTime())/(day))+366) + ' days left until Maths Day!';
@@ -39,7 +39,7 @@ function showAnswer() {
         magic.textContent = "(please select at least one card)";
         document.getElementById("gif").style.display = 'none';
     }
-}
+};
 
 function deleteAnswer(uncheckBoxes) {
     let verifyBoxes = document.getElementsByClassName("checkbox");
@@ -49,4 +49,12 @@ function deleteAnswer(uncheckBoxes) {
     magic.textContent = '';
     document.getElementById("gif").style.display = 'flex';
     document.getElementById("gif").style.width = "35";
-}
+};
+
+// let element = document.querySelectorAll(".nav-link");
+
+// element.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   document.getElementById("c1").style.color = "red";
+// }
